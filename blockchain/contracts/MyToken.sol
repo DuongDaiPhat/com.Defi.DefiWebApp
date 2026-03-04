9c4ef543a4b3fa70b1587df3dbdfe706016af59c
Trannguyen08 <trannguyen08py@gmail.com>
2026-03-04 22:46:08 +0700

    init core contract and env

--- blockchain/contracts/MyToken.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.20;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 1_000_000 * 10 ** decimals());  // khởi tạo 1 triệu token cho deployer
-    }
-}
