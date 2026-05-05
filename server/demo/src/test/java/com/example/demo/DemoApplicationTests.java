348ae3c9c73af6b4e4d90d3e243f0ef3a0c25fa9
DuongDaiPhat <duongphat521@gmail.com>
2026-05-05 23:38:02 +0700

    Perf: OSVA

--- server/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ server/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MSSQLServer",
         "spring.datasource.driverClassName=org.h2.Driver",
         "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
         "spring.datasource.username=sa",
         "spring.datasource.password="
 })
